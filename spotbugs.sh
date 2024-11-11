b5bc32c0dc2e98cfbf233ac50de0384a1a772718
Lorenzo Bevilacqua <43483037+ardubev16@users.noreply.github.com>
2024-11-12 00:49:23 +0100

    feat: add spotbugs

---
+++ spotbugs.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -e
+
+./spotbugs-4.8.6/bin/spotbugs -textui \
+    -html -output spotbugs.html \
+    -effort:max \
+    -progress \
+    -sourcepath ./Tourism/src/main/java \
+    ./Tourism/classes
